a0ebdf8b9c010c5d569747cf58e5e90c9c2d350c
Kotori316 <Kotori316@gmail.com>
2023-05-13 22:12:37 +0900

    Update for me!

--- README.md
+++ README.md
@@ -1,25 +1,50 @@
-# fabric-language-scala
-Fabric language module for [Scala](http://www.scala-lang.org/). Adds support for using a Scala `object` as the main mod class and bundles the Scala libraries for you.
+# Scalable Cat's Force Fabric
+
+Fabric language module for [Scala](http://www.scala-lang.org/). Adds support for using a Scala `object` as the main mod
+class and bundles the Scala libraries for you.
 
 ## Usage
+
 Add it as a dependency:
 
 ```groovy
 dependencies {
-	modImplementation "net.fabricmc:fabric-language-scala:0.3.1.+"
+    implementation(group: 'org.scala-lang', name: 'scala-library', version: "2.13.10")
+    // If you need scala3
+    implementation(group: 'org.scala-lang', name: 'scala3-library_3', version: "3.2.2")
+    // If you need cats. You also need to add repositories as below.
+    implementation(group: "org.typelevel", name: "cats-core_2.13", version: project.catsVersion)
+    implementation(group: "org.typelevel", name: "cats-kernel_2.13", version: project.catsVersion)
+    
+    modImplementation("com.kotori316:scalable-cats-force-fabric:1.0.0:dev")
+}
+
+// If you need cats.
+repositories {
+    maven {
+        name = "Azure Kotori316"
+        // See https://dev.azure.com/Kotori316/minecraft/_artifacts/feed/mods for versions
+        url = uri("https://pkgs.dev.azure.com/Kotori316/minecraft/_packaging/mods/maven/v1")
+        content {
+            it.includeVersion("org.typelevel", "cats-core_2.13", project.catsVersion)
+            it.includeVersion("org.typelevel", "cats-kernel_2.13", project.catsVersion)
+        }
+    }
 }
 ```
 
 Specify your entrypoint in your `fabric.mod.json` like so:
 
 ```json
-"entrypoints": {
+{
+  "entrypoints": {
     "main": [
-        {
-            "adapter": "scala",
-            "value": "package.ClassName"
-        }
+      {
+        "adapter": "scala",
+        "value": "package.ClassName"
+      }
     ]
+  }
 }
 ```
 
@@ -27,8 +52,8 @@ Add a dependency entry to your `fabric.mod.json` file:
 
 ```json
 {
-    "requires": {
-        "fabric-language-scala": "*"
-    }
+  "requires": {
+    "fabric-language-scala": "*"
+  }
 }
 ```
---
+++ src/main/java/com/kotori316/slp/ScalaLanguageAdapter.java
@@ -0,0 +1,74 @@
+package com.kotori316.slp;
+
+import net.fabricmc.loader.api.LanguageAdapter;
+import net.fabricmc.loader.api.LanguageAdapterException;
+import net.fabricmc.loader.api.ModContainer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class ScalaLanguageAdapter implements LanguageAdapter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ScalaLanguageAdapter.class);
+
+    /**
+     * Cases
+     * <ul>
+     *     <li>Just a class with default constructor</li>
+     *     <li>A scala object</li>
+     * </ul>
+     *
+     * @param mod   the mod which the object is from
+     * @param value the string declaration of the object
+     * @param type  the type that the created object must be an instance of
+     * @param <T>   the type, such as {@link net.fabricmc.api.ModInitializer}
+     * @return the mod instance
+     * @throws LanguageAdapterException if an error occurred in mod constructing
+     */
+    @Override
+    public <T> T create(ModContainer mod, String value, Class<T> type) throws LanguageAdapterException {
+        LOGGER.debug("Loading {} of  Scala mod({}) from {}", type, mod, value);
+        if (value.endsWith("$")) {
+            // Scala object
+            LOGGER.debug("({}) Detect object {}", mod, value);
+            try {
+                var obj = getScalaObject(mod, value);
+                LOGGER.debug("({}) Got object instance {}, interface={}", mod, obj, obj.getClass().getInterfaces());
+                return type.cast(obj);
+            } catch (ReflectiveOperationException | ClassCastException e) {
+                throw new LanguageAdapterException("Error in getting scala object of %s from %s".formatted(type, value), e);
+            }
+        } else {
+            LOGGER.debug("({}) Detect class {}", mod, value);
+            // Check object
+            try {
+                var clazz = Class.forName(value + "$", false, ScalaLanguageAdapter.class.getClassLoader());
+                LOGGER.debug("({}) Got object class {}", mod, clazz);
+                var obj = getScalaObject(mod, value + "$");
+                LOGGER.debug("({}) Got object instance {}, interface={}", mod, obj, obj.getClass().getInterfaces());
+                if (type.isInstance(obj)) {
+                    LOGGER.debug("({}) The object is implementing {}, return", mod, type);
+                    return type.cast(obj);
+                }
+            } catch (ReflectiveOperationException | ClassCastException ignore) {
+                LOGGER.debug("({}) {} doesn't have object for {}", mod, value, type);
+                // Object doesn't exist or object is not the class instance.
+            }
+
+            try {
+                var clazz = Class.forName(value, true, ScalaLanguageAdapter.class.getClassLoader());
+                LOGGER.debug("({}) Call constructor of {}", mod, value);
+                var constructor = clazz.getDeclaredConstructor();
+                var clsObject = constructor.newInstance();
+                LOGGER.debug("({}) Got class instance {}, interface={}", mod, clsObject, clsObject.getClass().getInterfaces());
+                return type.cast(clsObject);
+            } catch (ReflectiveOperationException | ClassCastException e) {
+                throw new LanguageAdapterException("Error in constructing an instance of %s from %s".formatted(type, value), e);
+            }
+        }
+    }
+
+    private static Object getScalaObject(ModContainer mod, String value) throws ReflectiveOperationException, ClassCastException {
+        LOGGER.debug("({}) Try to get MODULE$ from {}", mod, value);
+        var clazz = Class.forName(value, true, ScalaLanguageAdapter.class.getClassLoader());
+        return clazz.getField("MODULE$").get(null);
+    }
+}
