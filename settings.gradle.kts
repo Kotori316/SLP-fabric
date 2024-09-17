32450f63178a2c7191e22402408004fd060e6664
Kotori316 <Kotori316@gmail.com>
2024-09-18 00:44:16 +0900

    Register to Firestore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,10 @@ pluginManagement {
             name = "Fabric"
             url = uri("https://maven.fabricmc.net/")
         }
+        maven {
+            name = "Kotori316"
+            url = uri("https://maven.kotori316.com/")
+        }
         mavenCentral()
         gradlePluginPortal()
     }
