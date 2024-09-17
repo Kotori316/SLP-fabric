b894d4e3e75a5e74973e2ddd22bf8e37cc076ff4
Kotori316 <Kotori316@gmail.com>
2024-09-17 14:46:51 +0900

    Use version catalog to specify version

--- examples/settings.gradle.kts
+++ examples/settings.gradle.kts
@@ -9,6 +9,14 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version ("0.8.0")
 }
 
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
+
 rootProject.name = "SLP-fabric-examples"
 include("1.20.5")
 include("1.21.1")
